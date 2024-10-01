9ca523da2552836c029f6045eeec80b8a667a7e8
Porter Schollenberger <porterschollenberger@gmail.com>
2024-10-01 11:44:18 -0600

    add(authRouter.test.js) update user test

--- src/routes/authRouter.test.js
+++ src/routes/authRouter.test.js
@@ -3,11 +3,13 @@ const app = require('../service');
 
 const testUser = { name: 'pizza diner', email: 'reg@test.com', password: 'a' };
 let testUserAuthToken;
+let testUserId;
 
 beforeAll(async () => {
     testUser.email = Math.random().toString(36).substring(2, 12) + '@test.com';
     const registerRes = await request(app).post('/api/auth').send(testUser);
     testUserAuthToken = registerRes.body.token;
+    testUserId = registerRes.body.user.id;
 });
 
 test('login', async () => {
@@ -17,4 +19,15 @@ test('login', async () => {
 
     const { password, ...user } = { ...testUser, roles: [{ role: 'diner' }] };
     expect(loginRes.body.user).toMatchObject(user);
+});
+
+test('update user', async () => {
+    const updatedEmail = Math.random().toString(36).substring(2, 12) + '@test.com';
+    const updateRes = await request(app)
+        .put(`/api/auth/${testUserId}`)
+        .set('Authorization', `Bearer ${testUserAuthToken}`)
+        .send({ email: updatedEmail, password: 'newpassword' });
+
+    expect(updateRes.status).toBe(200);
+    expect(updateRes.body.email).toBe(updatedEmail);
 });
